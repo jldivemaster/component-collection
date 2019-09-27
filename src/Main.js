6dc1fc9de78f08a6c65df9677c910a7144107877
Jason Leach <jlspursfan@gmail.com>
2019-09-27 15:55:13 -0700

    completed season display comp

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import CommentCard from './components/CommentCard';
 import BGTransform from './components/BGTransform';
-import SeasonDisplay from './components/SeasonDisplay'
+import SeasonComponent from './components/SeasonComponent'
 
 export default class Main extends React.Component {
 
   render(){
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
-
     return (
       <div>
         <span>Comment Cards with Accept/Reject Btns</span>
@@ -21,10 +16,10 @@ export default class Main extends React.Component {
         <div className="bg">
         <BGTransform />
         </div>
-        <span>Displaying Season based on User Location</span>
+        <span>Displaying Season based on User Location w/ Loading Spinner (geolocation)</span>
         <br />
         <div className="season">
-        <SeasonDisplay />
+        <SeasonComponent />
         </div>
       </div>
     )
---
+++ src/components/SeasonComponent.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+export default class SeasonComponent extends React.Component {
+state = {
+      lat: null,
+      long: null,
+      errorMsg: ''
+    };
+
+
+  componentDidMount()  {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        })
+      },
+      err => {
+        this.setState({
+          errorMsg: err.message
+        })
+      }
+    );
+  }
+
+  renderContent = () => {
+    if(this.state.errorMsg && !this.state.lat){
+      return <div>Error: {this.state.errorMsg}</div>;
+    }
+
+    if(!this.state.errorMsg && this.state.lat){
+      return (<div>
+          <h3>Lat: {this.state.lat}</h3>
+          <h3>Long: {this.state.long}</h3>
+          <SeasonDisplay lat={this.state.lat} long={this.state.long}/>
+        </div>
+      )
+    }
+
+    return <Spinner message="Please accept location request" />
+  }
+
+  render(){
+    return (
+      <div className="bordered">
+        {this.renderContent()}
+      </div>
+    )
+  }
+}
